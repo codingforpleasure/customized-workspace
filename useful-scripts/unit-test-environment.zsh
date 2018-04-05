a9fa5f93612b35068d44973ec348b9342b369277
Gil <gil.cohen@blazemeter.com>
2018-04-05 12:52:18 +0300

    Have updated the bring-up unit-test environment

--- useful-scripts/unit-test-environment.zsh
+++ useful-scripts/unit-test-environment.zsh
@@ -1,11 +1,48 @@
 #!/bin/zsh
 
-# Restarting services:
+# I'm assuming you have installed mongodb@3.4,
+# if not, you should eneter enter:
+# brew install mongodb@3.4
 
-brew services stop mongodb@3.4
+# in case you have a different mongo version,
+# you should link to the correct version:
+# brew link mongodb@3.4 --force
+
+
+colorful_echo() 
+{
+	  local code="\033["
+		case "$1" in
+					red    ) color="${code}1;31m";;
+					green  ) color="${code}1;32m";;
+					yellow ) color="${code}1;33m";;
+					blue   ) color="${code}1;34m";;
+					purple ) color="${code}1;35m";;
+					cyan   ) color="${code}1;36m";;
+					gray   ) color="${code}0;37m";;
+					*) local text="$1"
+		esac
+		[ -z "$text" ] && local text="$color$2${code}0m"
+		echo -e "$text"
+}
+
+get_time=`date | awk '{print $4}'`	
+
+colorful_echo yellow "[${get_time}]: Bringing down local-db ( Both mongo server and Redis-server)"
+
+pkill -9 mongod
 pkill -9 redis-server
 
-# brew services start mongodb@3.4
-tmux new-window -t backend-work:7 -n 'unit-test' 'redis-server'
-tmux select-window -t backend-work:7
-tmux split-window -v 'brew services start mongodb@3.4'
+colorful_echo green "[${get_time}]: Bringing up local-db both mongo server and Redis-server."
+
+name='backend-work'
+window_number='7'
+window_name='db-unit-test'
+
+tmux new-window -t $name:$window_number -n $window_name
+tmux select-window -t $name:$window_number
+tmux split-window -v -t $name:$window_number
+
+# Now run the processess
+tmux send-keys -t $name:$window_number.1 'mongod --verbose' C-m #C-m is like pressing Enter
+tmux send-keys -t $name:$window_number.2 'redis-server' C-m
