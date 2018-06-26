dc014b7082c8936458050c26187973acdedf1ba9
codingforpleasure <cohengil333@gmail.com>
2018-06-26 11:06:55 +0300

    Linter capabilty was added for faster development stream

---
+++ Ubuntu/.config/sublime-text-3/Packages/User/linters/shellcheck.py
@@ -0,0 +1,20 @@
+from SublimeLinter.lint import Linter
+from shutil import which
+import sublime
+
+
+class Shellcheck(Linter):
+
+    cmd = 'shellcheck --format=gcc -'
+    if sublime.platform() == 'windows' and which('wsl'):
+        cmd = 'wsl ' + cmd
+
+    regex = (
+        r'^.+?:(?P<line>\d+):(?P<col>\d+): '
+        r'(?:(?P<error>error)|(?P<warning>(warning|note))): '
+        r'(?P<message>.+)$'
+    )
+    defaults = {
+        'selector': 'source.shell - source.makefile - source.shell.fish - text.html',
+        '--exclude=,': ''
+    }
