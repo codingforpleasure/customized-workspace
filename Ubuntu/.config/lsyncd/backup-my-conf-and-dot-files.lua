af3b14e27644d7187d1a45ab9163636c24bb0c2d
codingforpleasure <cohengil333@gmail.com>
2018-06-27 11:49:46 +0300

    Make lua script more generic and robust

--- Ubuntu/.config/lsyncd/backup-my-conf-and-dot-files.lua
+++ Ubuntu/.config/lsyncd/backup-my-conf-and-dot-files.lua
@@ -6,7 +6,36 @@
 
 -- Few remarks:
 -- If target is a local directory, take care that it is an absolute pathname
--- make sure the destination folders exist, otherwise it won't copy the files from the source
+
+
+
+
+function FolderExists(strFolderName)
+	local fileHandle, strError = io.open(strFolderName,"r")
+	if fileHandle ~= nil then
+		io.close(fileHandle)
+		return true
+	else
+		print(strError)
+		if string.match(strError,"No such file or directory") then
+			return false
+		else
+			return true
+		end
+	end
+end
+
+
+homeDir=os.getenv("HOME")
+destDir= "/dst-try/" -- IMPORTANT note: directory between SLASHES
+
+-- making sure the destination folders exist, otherwise it won't copy the files from the source
+if FolderExists(homeDir..destDir ..".config") == true then
+else
+	print(".config folder is created since it's missing.")
+	os.execute("mkdir -p ".. homeDir .. destDir .. ".config")
+end
+
 
 print("Backing-up your files!")
 
@@ -20,50 +49,42 @@ settings {
 -- 1) Backing-up all relevant precious dot files
 sync {
     default.direct,
-    source  = "/home/gil_diy/",
-    target  = "/home/gil_diy/dst-try",
-    rsync = { _extra = { "--files-from=/home/gil_diy/.config/lsyncd/files.list" } }
+    source  = homeDir,
+    target  = homeDir .. "/dst-try/",
+    rsync = { _extra = { "--files-from=" .. homeDir .. "/.config/lsyncd/files.list" } }
 }
 
 -- 2) Backing-up all multi-tail configurations files
 sync {
     default.direct,
-    source  = "/home/gil_diy/.config/multitail/",
-    target  = "/home/gil_diy/dst-try/.config/multitail",
+    source  = homeDir .. "/.config/multitail/",
+    target  = homeDir .. destDir .. ".config/multitail",
 }
 
 -- 3) Backing-up all existing project configuration files for tmuxinator
 sync {
     default.direct,
-    source  = "/home/gil_diy/.config/tmuxinator",
-    target  = "/home/gil_diy/dst-try/.config/tmuxinator",
+    source  = homeDir .. "/.config/tmuxinator",
+    target  = homeDir .. destDir .. ".config/tmuxinator",
 }
 
 -- 4) Backing-up all existing project configuration files for terminator
 sync {
     default.direct,
-    source  = "/home/gil_diy/.config/terminator",
-    target  = "/home/gil_diy/dst-try/.config/terminator",
+    source  = homeDir .. "/.config/terminator",
+    target  = homeDir .. destDir .. ".config/terminator",
 }
 
 -- 5) Backing-up sublime configurations
 sync {
     default.direct,
-    source  = "/home/gil_diy/.config/sublime-text-3/Packages/User",
-    target  = "/home/gil_diy/dst-try/.config/sublime-text-3/Packages/User",
+    source  = homeDir .. "/.config/sublime-text-3/Packages/User",
+    target  = homeDir .. destDir .. ".config/sublime-text-3/Packages/User",
 }
 
--- 6) Backing-up sublime configurations
+-- 6) Backing-up lsyncd configurations
 sync {
     default.direct,
-    source  = "/home/gil_diy/.config/sublime-text-3/Packages/User",
-    target  = "/home/gil_diy/dst-try/.config/sublime-text-3/Packages/User",
-}
-
-
--- 7) Backing-up lsyncd configurations
-sync {
-    default.direct,
-    source  = "/home/gil_diy/.config/lsyncd",
-    target  = "/home/gil_diy/dst-try/.config/lsyncd",
+    source  = homeDir .. "/.config/lsyncd",
+    target  = homeDir .. destDir .. ".config/lsyncd",
 }
