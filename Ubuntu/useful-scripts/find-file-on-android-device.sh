d19a068cea093165c53b341cf8047727e1b18562
codingforpleasure <cohengil333@gmail.com>
2018-07-17 19:09:42 +0300

    added capabilty for searching files on android device file-system

---
+++ Ubuntu/useful-scripts/find-file-on-android-device.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Searching for: $1, in your android device."
+
+adb shell ls -R / |
+    while read line
+    do
+            line=$(echo $line | tr -d '\r')
+
+            if [[ "$line" =~ ^/.*:$ ]]
+            then
+                    dir=${line%:}
+
+            elif [[ "$line" = "opendir failed, Permission denied" ]]
+            then
+                    echo "find: $dir: Permission denied"
+
+            else
+                if [[ "$dir" = "/" ]]; then dir=""; fi
+
+                echo "$dir/$line"
+            fi
+    done | grep -e "$1"
