5a2ec95198b3929a72bc7b17c1a355e587625e5a
codingforpleasure <cohengil333@gmail.com>
2018-08-07 03:16:38 +0300

    Runs on all over scripts which resides in current directory and changes mode to executable

---
+++ Ubuntu/useful-scripts/make-all-executable.zsh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Make sure all scripts(files) in current directory are executable
+
+colorful_echo()
+{
+	  local code="\033["
+		case "$1" in
+					red    ) color="${code}1;31m";;
+					green  ) color="${code}1;32m";;
+					yellow ) color="${code}1;33m";;
+					blue   ) color="${code}1;34m";;
+					purple ) color="${code}1;35m";;
+					cyan   ) color="${code}1;36m";;
+					gray   ) color="${code}0;37m";;
+					*) local text="$1"
+		esac
+		[ -z "$text" ] && local text="$color$2${code}0m"
+		echo -e "$text"
+}
+
+
+for SCRIPT in ./*
+do
+	if [ -f "$SCRIPT" ]
+	then
+		colorful_echo cyan "$SCRIPT is set to be executable file"
+		chmod +x $SCRIPT
+	fi
+done
