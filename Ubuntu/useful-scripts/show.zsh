c6fa19715c7085ce06a6d247f7453cfc1ff7df9c
codingforpleasure <cohengil333@gmail.com>
2018-08-24 19:16:01 +0300

    Fixed bugs in script

--- Ubuntu/useful-scripts/show.zsh
+++ Ubuntu/useful-scripts/show.zsh
@@ -25,7 +25,7 @@ NUM_OF_IMAGES=$(( $(feh -l *.png | wc -l ) -1))
 GRID_NUM_ROWS=${1:-2}     # First Argument is number of rows. Setting default to 3
 GRID_NUM_COLUMNS=${2:-3}  # Second Argument is number of columns. Setting default to 3
 TOTAL_PRESENTED_IN_GRID=$(( $GRID_NUM_ROWS * $GRID_NUM_COLUMNS ))
-NUM_ITERATIONS=$((NUM_OF_IMAGES / TOTAL_PRESENTED_IN_GRID + 1))
+NUM_ITERATIONS=$((NUM_OF_IMAGES / TOTAL_PRESENTED_IN_GRID))
 FILE_EXTENTION=${3:-png}
 THRESHOLD_WIDTH=60
 
@@ -37,28 +37,33 @@ if [[ $SINGLE_IMAGE_WIDTH -lt $THRESHOLD_WIDTH ]]; then
 fi
 
 SINGLE_IMAGE_HEIGHT=$(identify -ping -format '%h' ${FILE})
-PADDING_FIXED_WIDTH=$(( $GRID_NUM_COLUMNS*20 ))
-FIXED_WIDTH=$(( $SINGLE_IMAGE_WIDTH * $GRID_NUM_COLUMNS + $PADDING_FIXED_WIDTH + 100))
-FIXED_HEIGHT=$(( $SINGLE_IMAGE_HEIGHT * ($GRID_NUM_ROWS + 1) + 200  )) # Padding since I'm keeping space for captions
+FIXED_WIDTH=$(( $SINGLE_IMAGE_WIDTH * $GRID_NUM_COLUMNS ))
 FILENAME=$(echo montage-"$(date +"%T").png" | tr : -)
 
-colorful_echo yellow "A thumbnail file was exported: ${FILENAME}"
+#colorful_echo yellow "A thumbnail file was exported: ${FILENAME}"
 
 # Implementing a Sliding window for passing the corresoponding files to feh
-
-echo $NUM_ITERATIONS
-for (( i = 0; i <= $NUM_ITERATIONS; i++ )); do
+for (( i = 0; i < $NUM_ITERATIONS + 1; i++ )); do
+	LIST_OF_FILES=""
 	TAIL_LIST_LENGTH=$(( ${NUM_OF_IMAGES} - ${i}*${TOTAL_PRESENTED_IN_GRID} ))
-	LIST_OF_FILES=$( ls | tail -n ${TAIL_LIST_LENGTH} | head -n ${TOTAL_PRESENTED_IN_GRID} )
-
+	LIST_OF_FILES=$( ls | tail -n ${TAIL_LIST_LENGTH} | head -n ${TOTAL_PRESENTED_IN_GRID})
+	echo "${LIST_OF_FILES}" > "file_list${i}.txt"
+	NUM_OF_FILES=$(echo "$LIST_OF_FILES" | wc -w)
+	NUM_ROWS_NEW=$(( $NUM_OF_FILES / $GRID_NUM_COLUMNS ))
+	FIXED_HEIGHT=$(bc<<<"($NUM_ROWS_NEW + 2) * $SINGLE_IMAGE_HEIGHT")
+	FROM_IMG_NUM=$(( ${i}*${TOTAL_PRESENTED_IN_GRID} ))
+	UNTIL_IMG_NUM=$(( FROM_IMG_NUM + NUM_OF_FILES ))
 	feh --index \
+		--quiet \
 		--recursive \
 		--multiwindow \
 		--thumb-width "${SINGLE_IMAGE_WIDTH}" \
 		--thumb-height "${SINGLE_IMAGE_HEIGHT}"  \
 		--limit-width "${FIXED_WIDTH}" \
 		--limit-height "${FIXED_HEIGHT}" \
+		--title "${FROM_IMG_NUM} - ${UNTIL_IMG_NUM} images from total of ${NUM_OF_IMAGES} images (Dim: ${NUM_ROWS_NEW}x${GRID_NUM_COLUMNS}) ." \
+		--filelist file_list${i}.txt
 		#--output "./${FILENAME}"
-		${LIST_OF_FILES} &
 
+	rm file_list${i}.txt
 done
